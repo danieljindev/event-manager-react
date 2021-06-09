c44986f8077280419f58a6d30a02fb32cffaff80
Daniel Jin <danieljin123@outlook.com>
2021-06-09 06:15:26 +0300

    Add global state context to app.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,25 @@
 import Container from '@material-ui/core/Container'
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
+import { GlobalStateProvider } from 'src/contexts/GlobalStateContext'
 
 import About from './About'
 import Home from './Home'
 import NotFound from './NotFound'
 
-export default function SimpleContainer() {
+export default function App() {
   return (
     <>
-      <Container>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
-          <Redirect to="/" />
-        </Switch>
-      </Container>
+      <GlobalStateProvider>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+            <Redirect to="/" />
+          </Switch>
+        </Container>
+      </GlobalStateProvider>
     </>
   )
 }
